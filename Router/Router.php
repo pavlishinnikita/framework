68c02a42c3350ef0f4461193e2b5bd625db3fbae
PavlishinNikita <pavlishin.nikita@gmail.com>
2017-09-19 15:31:01 +0300

    Initial commit

---
+++ Router/Router.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace Framework\Router;
+
+use Cake\Collection\Collection;
+
+class Router
+{
+    private $rules = [];
+
+    public function get(string $query, string $action): bool
+    {
+        return $this->addRule($query, $action, 'get');
+    }
+
+    public function post(string $query, string $action): bool
+    {
+        return $this->addRule($query, $action, 'post');
+    }
+
+    public function find(string $query, string $method = 'get')
+    {
+        if(strlen($query)!=1) {
+            $query = rtrim($query,'/');
+        }
+
+        $rules = new Collection($this->rules);
+        $rule = $rules->filter(function ($rule) use ($query, $method) {
+            return $rule['method'] === $method && preg_match("#{$rule['query']}#", $query);
+        })->first();
+
+        if ($rule === null) {
+            return ["ErrorController@routeError"];
+        }
+
+        preg_match_all("/{$rule['query']}/", $query, $matches);
+        array_shift($matches);
+        $matches = new Collection($matches);
+        $matches = $matches->map(function ($item, $key) {
+            return array_values($item)[0] ?? null;
+        })->toArray();
+
+        array_unshift($matches, implode('@', [ $rule['controller'], $rule['action'] ]));
+
+        return $matches;
+    }
+
+    protected function addRule(string $query, string $action, string $method): bool
+    {
+        list($controller, $action) = explode('@', $action);
+        preg_match_all('#\{([0-9A-z]+)\}#', $query, $matches);
+        list($patterns, $variables) = $matches;
+
+        $query = preg_replace('#\{([0-9A-z]+)\}#', '([0-9A-z]+)', $query);
+
+        $query = str_replace('/', '\/', $query);
+        $query = "^{$query}$";
+
+        array_push($this->rules, [
+            'query' => $query,
+            'controller' => $controller,
+            'action' => $action,
+            'method' => $method
+        ]);
+
+        return true;
+    }
+
+}
