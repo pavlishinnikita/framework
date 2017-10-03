083e1ec215ab135572de532f471c64ebafe43c45
PavlishinNikita <pavlishin.nikita@gmail.com>
2017-10-03 16:01:00 +0300

    fixed bugs

---
+++ src/Database/DatabaseInterface.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: nikita
+ * Date: 9/29/17
+ * Time: 1:34 PM
+ */
+
+namespace Framework\Database;
+
+
+interface DatabaseInterface
+{
+
+}
---
+++ src/Database/MySQLDatabase.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: nikita
+ * Date: 9/29/17
+ * Time: 1:37 PM
+ */
+
+namespace Framework\Database;
+
+
+class MySQLDatabase
+{
+
+}
